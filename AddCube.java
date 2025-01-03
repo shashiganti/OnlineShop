dd403e0a2e5599e76209b13248f3b060103f186a
shashiganti <ganti22@gmail.com>
2025-01-02 20:01:49 -0500

    Add files via upload

---
+++ AddCube.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AddCube{
+
+public static void main(String args[]){
+
+Scanner in = new Scanner(System.in);
+System.out.println("Enter number of Iterations:  ");
+
+int n = in.nextInt();
+
+int sum = 0;
+
+for(int i=1;i<=n;i++){
+
+System.out.println(i*i*i);
+
+sum += i*i*i;
+
+}
+System.out.println("The total Sum of Cube Numbers:  " +sum);
+
+}
+}
